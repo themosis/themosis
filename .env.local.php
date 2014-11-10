6a122932edbb17c42c3a06051ff0f8d789aad771
Julien Lambé <julien@jlambe.be>
2014-11-10 16:58:41 +0100

    Update default gitignore behavior.

---
+++ .env.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+
+    'DB_NAME'       => 'database_name_here',
+    'DB_USER'       => 'database_user',
+    'DB_PASSWORD'   => 'database_password',
+    'DB_HOST'       => 'localhost',
+    'WP_HOME'       => 'http://example.com',
+    'WP_SITEURL'    => 'http://example.com/cms'
+
+);
