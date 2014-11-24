570c5e4e397fb50601bbf322632b8ac3b4f6a5f4
Julien Lambé <julien@jlambe.be>
2014-11-24 20:01:04 +0100

    Set the framework plugin in mu-plugins directory.

---
+++ htdocs/content/mu-plugins/load.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mu-Plugins Loader
+Description: Simple loader in order to run the Themosis framework core files.
+Author: Julien Lambé
+Version: 1.0
+Author URI: http://framework.themosis.com/
+*/
+require_once(WPMU_PLUGIN_DIR.'/themosis-framework/themosis.php');
