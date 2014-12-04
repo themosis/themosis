adcc3c8dcb2d33bf6261a787c8f447428744dafe
Julien Lambé <julien@jlambe.be>
2014-12-04 11:55:49 +0100

    Clean environment file.

--- config/environment.php
+++ config/environment.php
@@ -3,7 +3,9 @@
 /*----------------------------------------------------*/
 // Define your environments
 /*----------------------------------------------------*/
-return function()
-{
-    return ('apache_local' === getenv('APP_ENV')) ? 'local': 'production';
-};
+return array(
+
+    'local'         => 'your-local-hostname',
+    'production'    => 'your-production-hostname'
+
+);
