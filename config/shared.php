269e6886bf7c8892448dc2f0ca0f1ebfa39a082d
Ken Vu <ken@gladeye.co.nz>
2018-01-29 15:06:19 +1300

    Update `WP_DEFAULT_THEME` accordingly

--- config/shared.php
+++ config/shared.php
@@ -12,14 +12,14 @@ $table_prefix = getenv('DB_PREFIX') ? getenv('DB_PREFIX') : 'wp_';
 /*----------------------------------------------------*/
 $capsule = new Illuminate\Database\Capsule\Manager();
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => DB_HOST,
-    'database'  => DB_NAME,
-    'username'  => DB_USER,
-    'password'  => DB_PASSWORD,
-    'charset'   => DB_CHARSET,
+    'driver' => 'mysql',
+    'host' => DB_HOST,
+    'database' => DB_NAME,
+    'username' => DB_USER,
+    'password' => DB_PASSWORD,
+    'charset' => DB_CHARSET,
     'collation' => DB_COLLATE,
-    'prefix'    => $table_prefix
+    'prefix' => $table_prefix,
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
@@ -29,22 +29,22 @@ $GLOBALS['themosis.capsule'] = $capsule;
 // Authentication unique keys and salts
 /*----------------------------------------------------*/
 /*
- * @link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service 
+ * @link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
+define('AUTH_KEY', 'put your unique phrase here');
+define('SECURE_AUTH_KEY', 'put your unique phrase here');
+define('LOGGED_IN_KEY', 'put your unique phrase here');
+define('NONCE_KEY', 'put your unique phrase here');
+define('AUTH_SALT', 'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('LOGGED_IN_SALT', 'put your unique phrase here');
+define('NONCE_SALT', 'put your unique phrase here');
 
 /*----------------------------------------------------*/
 // Custom settings
 /*----------------------------------------------------*/
 define('WP_AUTO_UPDATE_CORE', false);
 define('DISALLOW_FILE_EDIT', true);
-define('WP_DEFAULT_THEME', 'themosis-theme');
+define('WP_DEFAULT_THEME', 'themosis');
 
 /* That's all, stop editing! Happy blogging. */
