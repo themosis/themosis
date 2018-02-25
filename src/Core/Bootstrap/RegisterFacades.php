5fd314e75fb6e59f7dc8e221b200564542183fa5
Julien Lambé <julien@themosis.com>
2018-02-25 19:16:36 +0100

    Implement alias loader.

--- src/Core/Bootstrap/RegisterFacades.php
+++ src/Core/Bootstrap/RegisterFacades.php
@@ -4,6 +4,7 @@ namespace Thms\Core\Bootstrap;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\Facade;
+use Thms\Core\AliasLoader;
 use Thms\Core\PackageManifest;
 
 class RegisterFacades
@@ -14,7 +15,9 @@ class RegisterFacades
 
         Facade::setFacadeApplication($app);
 
-        // TODO: Implement alias loader.
-        $aliases = $app[PackageManifest::class]->aliases();
+        AliasLoader::getInstance(array_merge(
+            $app['config']->get('app.aliases', []),
+            $app[PackageManifest::class]->aliases()
+        ))->register();
     }
 }
