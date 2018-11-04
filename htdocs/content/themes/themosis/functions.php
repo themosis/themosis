936fc6d46a928b137b9b129fe45438b97447a7a3
Julien Lambé <julien@themosis.com>
2018-11-04 19:22:10 +0100

    Remove old theme files.

--- htdocs/content/themes/themosis/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$app = app();
-
-add_action('template_redirect', function () use ($app) {
-    require $app->themesPath('themosis/routes.php');
-});
--- htdocs/content/themes/themosis/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Kernel;
-use Themosis\Core\Application;
-
-$app = Application::getInstance();
-$kernel = $app->make(Kernel::class);
-$response = $kernel->handle($app['request']);
-$response->send();
