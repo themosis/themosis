4a2b103c4e5be52c1646a55f7b2018c55b423360
Kevin Batdorf <kbat82@gmail.com>
2019-03-15 00:48:21 +0700

    comment out salts constants

--- config/wordpress.php
+++ config/wordpress.php
@@ -17,16 +17,18 @@
 |--------------------------------------------------------------------------
 |
 | @link https://api.wordpress.org/secret-key/1.1/salt/
+| Alternatively use the wp-cli command `wp config shuffle-salts`
+| to add salts to wp-config.php
 |
 */
-define('AUTH_KEY', 'put your unique phrase here');
-define('SECURE_AUTH_KEY', 'put your unique phrase here');
-define('LOGGED_IN_KEY', 'put your unique phrase here');
-define('NONCE_KEY', 'put your unique phrase here');
-define('AUTH_SALT', 'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT', 'put your unique phrase here');
-define('NONCE_SALT', 'put your unique phrase here');
+// define('AUTH_KEY', 'put your unique phrase here');
+// define('SECURE_AUTH_KEY', 'put your unique phrase here');
+// define('LOGGED_IN_KEY', 'put your unique phrase here');
+// define('NONCE_KEY', 'put your unique phrase here');
+// define('AUTH_SALT', 'put your unique phrase here');
+// define('SECURE_AUTH_SALT', 'put your unique phrase here');
+// define('LOGGED_IN_SALT', 'put your unique phrase here');
+// define('NONCE_SALT', 'put your unique phrase here');
 
 /*
 |--------------------------------------------------------------------------
