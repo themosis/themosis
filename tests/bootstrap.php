c6267abaa60e884eb1bb2328356137718d512dc8
Julien Lambé <julien@themosis.com>
2019-03-27 17:16:51 +0100

    Fix #279. Incorrect themosis root path.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ defined('DS') ? DS : define('DS', DIRECTORY_SEPARATOR);
 // Application paths
 /*----------------------------------------------------*/
 define('THEMOSIS_PUBLIC_DIR', 'htdocs');
-define('THEMOSIS_ROOT', realpath(__DIR__));
+define('THEMOSIS_ROOT', realpath(__DIR__.'/../'));
 define('CONTENT_DIR', 'content');
 define('WP_CONTENT_DIR', realpath(THEMOSIS_ROOT.DS.THEMOSIS_PUBLIC_DIR.DS.CONTENT_DIR));
 
