8851f98b715be22806bd27f5277c9633b4171a5c
Julien Lambé <julien@themosis.com>
2019-12-20 16:26:11 +0100

    Comment broadcast service provider by default.

--- config/app.php
+++ config/app.php
@@ -155,7 +155,7 @@ return [
         // Application providers
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
