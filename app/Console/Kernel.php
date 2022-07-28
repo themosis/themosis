8932271f02c9b7ab81c73d6a78d0305976a34219
Julien <julien@themosis.com>
2022-07-28 06:00:16 +0200

    Add missing environment configuration. Disable cron in order to support artisan scheduler.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('publish:future-posts')->twiceDaily();
     }
 
     /**
